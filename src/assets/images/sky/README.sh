7b4047a6f3e4df63769e37c70607e895c32bb19d
lc4t <lc4t0.0@gmail.com>
2023-07-07 13:52:28 +0800

    feat: add sky image

--- src/assets/images/sky/README.sh
+++ src/assets/images/sky/README.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 brew install exiftool imagemagick jq exiv2
-exiftool {name} -j| jq '.[0].CreateDate'
-mogrify -format jpg *.DNG
+for fname in `ls *.JPG`; do
+  newname=`exiftool $fname -j| jq '.[0].CreateDate' | sed 's/\:/-/g' | sed 's/"//g'`
+  mogrify -format jpg $fname
+  mv ${fname%%.*}.jpg "$newname.jpg" && rm -f $fname
+  echo "$fname => $newname.jpg"
+done
+# exiftool {name} -j| jq '.[0].CreateDate'
+# mogrify -format jpg *.DNG
 exiv2 rm *.jpg
